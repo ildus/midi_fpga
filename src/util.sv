df64f35a4d6874c1767e98e690eb91a086452004
Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
2021-01-28 18:59:30 +0500

    files

---
+++ src/util.sv
@@ -0,0 +1,45 @@
+`define CLOG2(x) \
+   x <= 2	 ? 1 : \
+   x <= 4	 ? 2 : \
+   x <= 8	 ? 3 : \
+   x <= 16	 ? 4 : \
+   x <= 32	 ? 5 : \
+   x <= 64	 ? 6 : \
+   x <= 128	 ? 7 : \
+   x <= 256	 ? 8 : \
+   x <= 512	 ? 9 : \
+   x <= 1024	 ? 10 : \
+   x <= 2048	 ? 11 : \
+   x <= 4096	 ? 12 : \
+   x <= 8192	 ? 13 : \
+   x <= 16384	 ? 14 : \
+   x <= 32768	 ? 15 : \
+   x <= 65536	 ? 16 : \
+   x <= 131072	 ? 18 : \
+   x <= 262144	 ? 18 : \
+   -1
+
+module divide_by_n(
+	input clk,
+	input rst,
+	output reg out
+);
+	parameter N = 2;
+
+	reg [`CLOG2(N)-1:0] counter;
+
+	always @(posedge clk)
+	begin
+		out <= 0;
+
+		if (rst)
+			counter <= 0;
+		else
+		if (counter == 0)
+		begin
+			out <= 1;
+			counter <= N - 1;
+		end else
+			counter <= counter - 1;
+	end
+endmodule
